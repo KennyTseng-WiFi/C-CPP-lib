b45971256dfd893713c11ee8e1bdc46d8587adab
easy <easytzeng@gmail.com>
2020-07-25 14:06:47 +0800

    print on AP console

---
+++ console_print.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define CONSOLE_PRINT(format, args...)                                do{\
+	int console_fd;\
+	if ((console_fd = open ("/dev/console", O_WRONLY)) == -1) \
+	{\
+		fprintf(stderr, "Failed to open console.\n");\
+		exit (1);\
+	}\
+	char buf2[256];                         \
+	sprintf(buf2, format "\n", ## args);         \
+	write(console_fd, buf2, strlen(buf2));  \
+	close(console_fd);\
+}while(0)
+
+CONSOLE_PRINT("%s-%d: call\n", __func__, __LINE__);
+
+
